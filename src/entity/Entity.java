94b0848df46105aaa75775908e0f779ab1cd4830
Tyler Morgan <tyler.morgan@richmond.edu>
2013-01-08 16:31:14 -0600

    1/8/13

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,13 @@
 package entity;
 
+import java.util.Random;
+
+import level.Level;
+
 public class Entity {
 
+	public int x, y;
+	public boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
 }
